b66a722f45393970212efc96885c15079b3ef1e9
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2021-05-16 01:27:33 +0530

    api: added stubs for various API functions to be implemented

--- api/src/constituency.py
+++ api/src/constituency.py
@@ -12,4 +12,22 @@ def getConstituencyFromLocation():
 		return jsonify({'error':"request body must contain float type fields 'latitude' and 'longitude'"}), 400
 	elif not isinstance(lat, float) or not isinstance(lon, float):
 		return jsonify({'error':"fields 'latitude' and 'longitude' must be of type float"}), 400
-	return jsonify({'l': [lat, str(type(lat))], 'h':[lon, isinstance(lon, float)]})
+	
+	# TODO: Implement the function
+	return jsonify({'error':'Function not implemented'})
+
+@app.route('/getConstituencyFromVoterId', methods=['GET','POST'])
+def getConstituencyFromVoterId():
+	voter_id = request.json.get('voter_id') if request.method == 'POST' else request.args.get('voter_id')
+	if not voter_id:
+		return jsonify({'error':"request body must contain the field 'voter_id'"}), 400
+	elif not isinstance(lat, str):
+		return jsonify({'error':"field 'voter_id' must be of type str"}), 400
+	
+	# TODO: Implement the function
+	return jsonify({'error':'Function not implemented'})
+
+@app.route('/getConstituencyFromVoterIdCardImage', methods=['POST'])
+def getConstituencyFromVoterIdCardImage():	
+	# TODO: Implement the function
+	return jsonify({'error':'Function not implemented'})
---
+++ api/src/election.py
@@ -0,0 +1,12 @@
+from src import app
+from flask import jsonify, request
+
+@app.route('/getElectionsFromConstituency', methods['GET', 'POST'])
+def getElectionsFromConstituency():
+    # TODO: Implement the function
+	return jsonify({'error':'Function not implemented'})
+
+@app.route('/getElectionsFromPartNo', methods['GET', 'POST'])
+def getElectionsFromPartNo():
+    # TODO: Implement the function
+	return jsonify({'error':'Function not implemented'})
