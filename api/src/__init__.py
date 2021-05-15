791c6de847aead08135404abcb98eada5b481ba6
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2021-05-16 00:24:35 +0530

    api: removed experimental code from src/__init__.py

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -7,9 +7,4 @@ dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
 if os.environ.get('IS_OFFLINE'):
 	dynamodb_client = boto3.client('dynamodb', region_name='localhost', endpoint_url='http://localhost:8000')
 
-USERS_TABLE = os.environ['USERS_TABLE']
-dynamodb_client.put_item(
-	TableName=USERS_TABLE, Item={'userId': {'S': "1"}, 'name': {'S': "Avi"}}
-)
-
 from src import endpoints
